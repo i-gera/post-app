a740a7a39c6b7eb4b44ba4f7b1c7f1b0e4935010
i_gera <gera_0607@bk.ru>
2020-05-20 01:28:06 +0600

    Updates

---
+++ precache-manifest.6cdf99bdec3c6fd8d80d116b36d95952.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016add375f18d32fc5be293e9f1e0991",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73f7fcd33c222f3cfa1c",
+    "url": "/static/css/main.b7182535.chunk.css"
+  },
+  {
+    "revision": "07ca3a548a597ba75783",
+    "url": "/static/js/2.aaa7a33e.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.aaa7a33e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f7fcd33c222f3cfa1c",
+    "url": "/static/js/main.fb0aa966.chunk.js"
+  },
+  {
+    "revision": "18cd307b9c6f01c65977",
+    "url": "/static/js/runtime-main.9db5a6b7.js"
+  },
+  {
+    "revision": "ee61ca49ec2188e4ac026017e8b315b7",
+    "url": "/static/media/logo.ee61ca49.svg"
+  },
+  {
+    "revision": "e090b2b4e0c0b43377a1c82d791c4329",
+    "url": "/static/media/preloader.e090b2b4.svg"
+  },
+  {
+    "revision": "d00d5149e54757861fa03191da352f1f",
+    "url": "/static/media/user.d00d5149.png"
+  }
+]);
